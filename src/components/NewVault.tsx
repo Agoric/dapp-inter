df9f3d4819b2f73b466adcf3b6c871fe001b7624
samsiegart <samsiegart@gmail.com>
2023-01-03 12:56:27 -0800

    feat: add new vault button

---
+++ src/components/NewVault.tsx
@@ -0,0 +1,34 @@
+import { useVaultStore } from 'store/vaults';
+
+const NewVault = ({ id }: { id: string }) => {
+  const {
+    vaultGovernedParams,
+    vaultLoadingErrors,
+    vaultManagers,
+    vaultMetrics,
+    prices,
+    priceErrors,
+  } = useVaultStore();
+
+  const manager = vaultManagers.get(id);
+  const metrics = vaultMetrics.get(id);
+  const params = vaultGovernedParams.get(id);
+  const brand = metrics?.retainedCollateral?.brand;
+  const price = brand && prices.get(brand);
+
+  const error = vaultLoadingErrors.get(id) || (brand && priceErrors.get(brand));
+
+  const isLoading =
+    !(manager && metrics && params && price) || error != undefined;
+
+  return (
+    <button
+      className="bg-purple-500 hover:bg-purple-700 text-white font-semibold py-2 px-4 rounded"
+      disabled={isLoading}
+    >
+      Borrow 10 IST
+    </button>
+  );
+};
+
+export default NewVault;
