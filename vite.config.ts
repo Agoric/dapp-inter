be18393babef23796b6a86150a35815b43916a8a
Ari Rubinstein <ari@agoric.com>
2023-01-26 21:30:35 -0800

    fix: modify base root

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   optimizeDeps: {
     include: ['@endo/init'],
     esbuildOptions: { target: 'es2020', supported: { bigint: true } },
