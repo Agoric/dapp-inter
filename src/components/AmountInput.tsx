495bcbcd67eb8b29908fa56d82e211756795d6b7
samsiegart <samsiegart@gmail.com>
2023-02-01 15:51:17 -0800

    chore: document AmountInput trickery

--- src/components/AmountInput.tsx
+++ src/components/AmountInput.tsx
@@ -35,6 +35,8 @@ const AmountInput = ({
   );
 
   const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = ev => {
+    // Inputs with type "number" allow these characters which don't apply to
+    // Amounts, just strip them.
     const str = ev.target?.value?.replace('-', '').replace('e', '');
     setFieldString(str);
 
@@ -42,6 +44,13 @@ const AmountInput = ({
     onChange(parsed);
   };
 
+  // Use the `fieldString` as an input buffer so the user can type values that
+  // would be overwritten by `stringifyValue`. For example, if the current
+  // input is "1.05", and you tried to change it to "1.25", on hitting
+  // backspace, `stringifyValue` would change it from "1.0" to "1.00",
+  // preventing you from ever editing it. Only let `amountString` override
+  // `fieldString` if the controlled input is trying to change it to a truly
+  // different value.
   const displayString =
     value === parseAsValue(fieldString, AssetKind.NAT, decimalPlaces)
       ? fieldString
