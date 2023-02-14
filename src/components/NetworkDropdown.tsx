a69c2f21cfbed4d046e5bf0eaaef5f79cd49bbe7
Ari Rubinstein <arirubinstein@users.noreply.github.com>
2023-02-14 12:58:42 -0800

    feat: allow queryparam netselect (#71)
    * fix: allow queryparam netselect
    
    * fix: add log
    
    * fix: format

--- src/components/NetworkDropdown.tsx
+++ src/components/NetworkDropdown.tsx
@@ -32,6 +32,19 @@ const Item = ({
 
 const NetworkDropdown = () => {
   const [networkConfig, setNetworkConfig] = useAtom(networkConfigAtom);
+  const specifiedNetworkName = new URLSearchParams(window.location.search).get(
+    'network',
+  );
+  if (specifiedNetworkName !== null) {
+    if (specifiedNetworkName in networkConfigs) {
+      setNetworkConfig((networkConfigs as never)[specifiedNetworkName]);
+      const prevSearch = new URLSearchParams(window.location.search);
+      prevSearch.delete('network');
+      window.location.replace(`?${prevSearch}`);
+    } else {
+      console.error('invalid network requested', specifiedNetworkName);
+    }
+  }
 
   const items = Object.values(networkConfigs).map(config => (
     <Item
