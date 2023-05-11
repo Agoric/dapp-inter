a40c74cf3c9f39feee5004d0584cb6b77e64d0f9
samsiegart <samsiegart@gmail.com>
2023-05-11 13:51:05 -0700

    feat: tighten up vertical space

--- src/components/CreateVault.tsx
+++ src/components/CreateVault.tsx
@@ -20,10 +20,8 @@ const CreateVault = () => {
 
   return (
     <>
-      <div className="flex justify-between mt-6 flex-wrap gap-4">
-        <div className="font-serif font-medium text-2xl">
-          Creating New Vault
-        </div>
+      <div className="flex justify-between mt-4 flex-wrap gap-4">
+        <div className="font-serif font-medium text-2xl">Choose Collateral</div>
         <button
           className="text-btn-xs transition mr-1 text-[#A3A5B9] rounded-[6px] border-2 border-solid border-[#A3A5B9] py-3 px-7 leading-[14px] font-bold text-xs bg-gray-500 bg-opacity-0 hover:bg-opacity-10 active:bg-opacity-20"
           onClick={buttonProps.onClick}
@@ -31,11 +29,8 @@ const CreateVault = () => {
           {buttonProps.text}
         </button>
       </div>
-      <div className="mt-16 grid grid-cols-11 lg:gap-x-[51px]">
+      <div className="mt-8 grid grid-cols-11 lg:gap-x-[51px]">
         <div className="col-span-11 lg:col-span-7">
-          <div className="font-serif font-medium text-2xl mb-8">
-            Choose Collateral
-          </div>
           <div className="flex flex-row flex-wrap gap-[18.5px]">
             {vaultManagerIds && displayFunctions ? (
               vaultManagerIds.map(id => (
