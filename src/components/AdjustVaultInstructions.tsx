68e901b730bdec11e7fa89fbe15b03e96e6eb2b9
samsiegart <samsiegart@gmail.com>
2023-05-25 10:04:54 -0700

    chore: remove unnecessary default color specificiation

--- src/components/AdjustVaultInstructions.tsx
+++ src/components/AdjustVaultInstructions.tsx
@@ -1,6 +1,6 @@
 export const AdjustVaultInstructions = () => (
   <div className="bg-white shadow-[0_22px_34px_rgba(116,116,116,0.25)] rounded-[10px] px-7 py-4">
-    <h3 className="text-[#484848] font-bold font-serif">Instructions</h3>
+    <h3 className="font-bold font-serif">Instructions</h3>
     <ol className="list-decimal text-[#A3A5B9] text-sm mx-4 my-3 [&>li]:mt-1">
       <li>
         Select at least one action from either dropdown (you can complete a debt
