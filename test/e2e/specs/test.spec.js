c55480b72338c9816da198b264455f48c09469f8
Fraz Arshad <fraz.arshad12@gmail.com>
2024-03-28 15:36:23 +0500

    test: adjusted tests to be cyclic

--- test/e2e/specs/test.spec.js
+++ test/e2e/specs/test.spec.js
@@ -47,6 +47,10 @@ describe('Vaults UI Test Cases', () => {
           'You can manage your vaults from the "My Vaults" view.',
         ).should('exist');
       });
+      cy.get('label.cursor-pointer input[type="checkbox"]').check();
+      cy.contains('Proceed').click();
+
+      cy.acceptAccess();
     });
 
     it('should adjust the collateral by performing a withdrawl and approve the transaction successfully', () => {
@@ -64,6 +68,85 @@ describe('Vaults UI Test Cases', () => {
             .within(() => {
               cy.get('input[type="number"]').click();
               cy.get('input[type="number"]').type(5);
+            });
+        });
+
+      cy.contains('button', 'Adjust Vault').click();
+      cy.confirmTransaction().then(taskCompleted => {
+        expect(taskCompleted).to.be.true;
+        cy.contains('p', "Your vault's balances have been updated.").should(
+          'exist',
+        );
+        cy.contains('Back to my vaults').click();
+      });
+    });
+
+    it('should create a new vault and approve the transaction successfully', () => {
+      cy.visit('/');
+
+      cy.contains('button', 'Add new vault').click();
+      cy.contains('button', /ATOM/).click();
+
+      cy.contains('.input-label', 'ATOM to lock up *')
+        .next()
+        .within(() => {
+          cy.get('input[type="number"]').click();
+          cy.get('input[type="number"]').clear();
+          cy.get('input[type="number"]').type(2);
+        });
+
+      cy.contains('button', 'Create Vault').click();
+
+      cy.confirmTransaction().then(taskCompleted => {
+        expect(taskCompleted).to.be.true;
+        cy.contains(
+          'p',
+          'You can manage your vaults from the "My Vaults" view.',
+        ).should('exist');
+      });
+    });
+
+    it('should open the new vault', () => {
+      cy.visit('/');
+      cy.get('span')
+        .contains(/My Vaults.*\(\d+\)/)
+        .children()
+        .first()
+        .spread((...element) => {
+          // Get the total number of vaults present
+          const vaultCount = Number(element[0].innerHTML.slice(2, -1));
+
+          cy.get('div.shadow-card div.text-secondary:contains("#")')
+            .should('have.length', vaultCount)
+            .spread((...vaults) => {
+              // Get the vault with the largest number and click on it
+              const maxValue = vaults.reduce(
+                (maxValue, currentValueRaw) => {
+                  const currentValue = Number(
+                    currentValueRaw.innerHTML.slice(1),
+                  );
+                  return currentValue > maxValue ? currentValue : maxValue;
+                },
+                Number(vaults[0].innerHTML.slice(1)),
+              );
+              cy.get(
+                `div.shadow-card div.text-secondary:contains("#${maxValue}")`,
+              ).click();
+            });
+        });
+    });
+
+    it('should adjust the collateral by performing a withdrawl and approve the transaction successfully', () => {
+      cy.contains('div', 'Adjust Collateral')
+        .next('.grid-cols-2')
+        .within(() => {
+          cy.contains('button', /^(Deposit|Withdraw|No Action)$/).click();
+          cy.contains('button', 'Withdraw').click();
+          cy.contains('.input-label', 'Amount')
+            .next('.input-wrapper')
+            .within(() => {
+              cy.get('input[type="number"]').click();
+              cy.get('input[type="number"]').type(1);
             });
         });
 
@@ -151,9 +234,6 @@ describe('Vaults UI Test Cases', () => {
     });
 
     it('should close the vault and approve the transaction successfully', () => {
-      cy.visit('/');
-      cy.contains('div', /ATOM.*#8/).click();
-
       cy.contains('Close Out Vault').click();
       cy.contains('button.bg-interPurple', 'Close Out Vault').click();
 
