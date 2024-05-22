066470e53ddfd010204165f3453ef8d40d94cbf5
rabi-siddique <rabisiddique27@gmail.com>
2024-05-22 13:54:59 +0500

    tests: add tests for setting up governance params

--- test/e2e/specs/liquidation-reconstitution.spec.js
+++ test/e2e/specs/liquidation-reconstitution.spec.js
@@ -10,6 +10,12 @@ describe('Wallet App Test Cases', () => {
     // Using exports from the synthetic-chain lib instead of hardcoding mnemonics UNTIL https://github.com/Agoric/agoric-3-proposals/issues/154
     it('should set up wallets', () => {
       cy.setupWallet({
+        secretWords: mnemonics.user1,
+        walletName: 'user1',
+      }).then(taskCompleted => {
+        expect(taskCompleted).to.be.true;
+      });
+      cy.setupWallet({
         secretWords: mnemonics.gov1,
         walletName: 'gov1',
       }).then(taskCompleted => {
@@ -21,17 +27,210 @@ describe('Wallet App Test Cases', () => {
       }).then(taskCompleted => {
         expect(taskCompleted).to.be.true;
       });
-      cy.setupWallet({
-        secretWords: mnemonics.user1,
-        walletName: 'user1',
-      }).then(taskCompleted => {
-        expect(taskCompleted).to.be.true;
-      });
+    });
+  });
+
+  context('Adjusting manager params from econ-gov', () => {
+    it('should connect with chain and wallet', () => {
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+      cy.acceptAccess();
+    });
+    it('should allow gov2 to create a proposal', () => {
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+      cy.acceptAccess();
+
+      cy.get('button').contains('Vaults').click();
+      cy.get('button').contains('Select Manager').click();
+      cy.get('button').contains('manager0').click();
+
+      cy.get('label')
+        .contains('LiquidationMargin')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('150');
+        });
+
+      cy.get('label')
+        .contains('LiquidationPadding')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('25');
+        });
+
+      cy.get('label')
+        .contains('LiquidationPenalty')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('1');
+        });
+
+      cy.get('label')
+        .contains('StabilityFee')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('1');
+        });
+
+      cy.get('label')
+        .contains('MintFee')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('0.5');
+        });
+
+      cy.get('label')
+        .contains('Minutes until close of vote')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type(1);
+        });
+      cy.get('[value="Propose Parameter Change"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p')
+        .contains('sent')
+        .should('be.visible')
+        .then(() => {
+          startTime = Date.now();
+        });
+    });
+
+    it('should allow gov2 to vote on the proposal', () => {
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+
+      cy.get('button').contains('Vote').click();
+      cy.get('p').contains('YES').click();
+      cy.get('input:enabled[value="Submit Vote"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p').contains('sent').should('be.visible');
+    });
+
+    it('should allow gov1 to vote on the proposal', () => {
+      cy.switchWallet('gov1');
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+
+      cy.get('button').contains('Vote').click();
+      cy.get('p').contains('YES').click();
+      cy.get('input:enabled[value="Submit Vote"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p').contains('sent').should('be.visible');
+    });
+
+    it('should wait for proposal to pass', () => {
+      cy.wait(60000 - Date.now() + startTime);
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+
+      cy.get('button').contains('History').click();
+
+      cy.get('code')
+        .contains('VaultFactory - ATOM')
+        .parent()
+        .parent()
+        .parent()
+        .within(() => {
+          cy.get('span').contains('Change Accepted').should('be.visible');
+        });
+    });
+  });
+
+  context('Adjusting auction params from econ-gov', () => {
+    it('should allow gov1 to create a proposal', () => {
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+
+      cy.get('button').contains('Vaults').click();
+      cy.get('button').contains('Change Manager Params').click();
+      cy.get('button').contains('Change Auctioneer Params').click();
+
+      cy.get('label')
+        .contains('StartingRate')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('10500');
+        });
+
+      cy.get('label')
+        .contains('LowestRate')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('6500');
+        });
+
+      cy.get('label')
+        .contains('DiscountStep')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('500');
+        });
+
+      cy.get('label')
+        .contains('AuctionStartDelay')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('2');
+        });
+
+      cy.get('label')
+        .contains('Minutes until close of vote')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type(1);
+        });
+      cy.get('[value="Propose Parameter Change"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p')
+        .contains('sent')
+        .should('be.visible')
+        .then(() => {
+          startTime = Date.now();
+        });
+    });
+
+    it('should allow gov1 to vote on the proposal', () => {
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+
+      cy.get('button').contains('Vote').click();
+      cy.get('p').contains('YES').click();
+      cy.get('input:enabled[value="Submit Vote"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p').contains('sent').should('be.visible');
+    });
+
+    it('should allow gov2 to vote on the proposal', () => {
+      cy.switchWallet('gov2');
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+
+      cy.get('button').contains('Vote').click();
+      cy.get('p').contains('YES').click();
+      cy.get('input:enabled[value="Submit Vote"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p').contains('sent').should('be.visible');
+    });
+
+    it('should wait for proposal to pass', () => {
+      cy.wait(60000 - Date.now() + startTime);
+      cy.visit('https://econ-gov.inter.trade/?agoricNet=local');
+
+      cy.get('button').contains('History').click();
+
+      cy.get('code')
+        .contains('VaultFactory - ATOM')
+        .parent()
+        .parent()
+        .parent()
+        .within(() => {
+          cy.get('span').contains('Change Accepted').should('be.visible');
+        });
     });
   });
 
   context('Creating vaults and changing ATOM price', () => {
     it('should connect with the wallet', () => {
+      cy.switchWallet('user1');
       cy.visit('/');
 
       cy.contains('Connect Wallet').click();
