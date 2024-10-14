afff30c857bbe86eff16b72efa44af377547052a
rabi-siddique <rabisiddique27@gmail.com>
2024-10-14 08:23:14 +0500

    testt

--- test/e2e/specs/liquidation.spec.js
+++ test/e2e/specs/liquidation.spec.js
@@ -16,6 +16,33 @@ describe('Wallet App Test Cases', () => {
   const AGORIC_NET = Cypress.env('AGORIC_NET');
   const network = AGORIC_NET !== 'local' ? 'testnet' : 'local';
   const currentConfig = configMap[network];
+  const user1Mnemonic = currentConfig.user1Mnemonic;
+  const user1Address = currentConfig.user1Address;
+  const bidderMnemonic = currentConfig.bidderMnemonic;
+  const bidderAddress = currentConfig.bidderAddress;
+  const bidderWalletName = currentConfig.bidderWalletName;
+  const gov1Mnemonic = currentConfig.gov1Mnemonic;
+  const gov1Address = currentConfig.gov1Address;
+  const gov2Mnemonic = currentConfig.gov2Mnemonic;
+  const gov2Address = currentConfig.gov2Address;
+
+  it('should add all the keys successfully', () => {
+    cy.addKeys({
+      keyName: 'gov1',
+      mnemonic: gov1Mnemonic,
+      expectedAddress: gov1Address,
+    });
+    cy.addKeys({
+      keyName: 'gov2',
+      mnemonic: gov2Mnemonic,
+      expectedAddress: gov2Address,
+    });
+    cy.addKeys({
+      keyName: 'user1',
+      mnemonic: user1Mnemonic,
+      expectedAddress: user1Address,
+    });
+  });
 
 
   context('WWW', () => {
